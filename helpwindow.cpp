31baaf5cf9a8246871200fba36432f09e6b90e46
zxt <zxt@pku.edu.cn>
2017-01-05 15:26:44 +0800

    add help content

--- helpwindow.cpp
+++ helpwindow.cpp
@@ -1,12 +1,25 @@
 #include "helpwindow.h"
 #include "ui_helpwindow.h"
+#include <QFile>
+#include <iostream>
+using namespace std;
 
 HelpWindow::HelpWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::HelpWindow)
 {
     ui->setupUi(this);
-    ui->helpBR->insertHtml("<b>hello,world</b>");
+    QFile file("help/help.html");
+    file.open(QIODevice::ReadOnly);
+    //string str=file.readAll().toStdString();
+
+    ui->helpBR->insertHtml(file.readAll());
+    file.close();
+
+    QTextCursor cursor=ui->helpBR->textCursor();
+    cursor.setPosition(0);
+    ui->helpBR->setTextCursor(cursor);
+
 }
 
 HelpWindow::~HelpWindow()
